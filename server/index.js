549d832aff9841f1b5df0c6dbc2f73d415aa283c
Emanuel Leite <emanuel.leite10@outlook.com>
2024-01-10 15:57:05 -0600

    Testing Multer

--- server/index.js
+++ server/index.js
@@ -1,54 +1,50 @@
 const express = require('express');
 const app = express();
-const multer = require("multer");
 const path = require("path");
 const cors = require('cors')
+const multer = require("multer");
+
 
 // Whitelist API
 app.use(cors());
 
-// storage engine 
+// Storage engine 
+const storage = multer.diskStorage({
+    destination: './upload',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
 
-// const storage = multer.diskStorage({
-//     destination: './upload',
-//     filename: (req, file, cb) => {
-//         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-//     }
-// })
+const upload = multer({
+    storage: storage,
+})
 
-// const upload = multer({
-//     storage: storage,
-// })
+app.use('/images', express.static('upload'));
+app.post("/upload", upload.single('image'), (req, res) => {
+    res.json({
+        profile_url: `http://localhost:3000/images/${req.file.filename}`
+    })
+})
 
-// app.use('/images', express.static('upload'));
-// app.post("/upload", upload.single('image'), (req, res) => {
-
-//     res.send("Send images to this endpoint via post");
-
-//     res.json({
-//         success: 1,
-//         profile_url: `http://localhost:3000/images/${req.file.filename}`
-//     })
-// })
-
-// function errHandler(err, req, res, next) {
-//     if (err instanceof multer.MulterError) {
-//         res.status(400).json({
-//             success: 0,
-//             message: err.message
-//         });
-//     } else if (err instanceof CustomError) {
-//         res.status(300).json({
-//             success: 0,
-//             message: err.message
-//         });
-//     } else {
-//         res.status(500).json({
-//             success: 0,
-//             message: 'Error interno del servidor'
-//         });
-//     }
-// }
+function errHandler(err, req, res, next) {
+    if (err instanceof multer.MulterError) {
+        res.status(400).json({
+            success: 0,
+            message: err.message
+        });
+    } else if (err instanceof CustomError) {
+        res.status(300).json({
+            success: 0,
+            message: err.message
+        });
+    } else {
+        res.status(500).json({
+            success: 0,
+            message: 'Error interno del servidor'
+        });
+    }
+}
 
 
 app.get("/api", (req, res) => {
