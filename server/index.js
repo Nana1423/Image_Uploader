ef276960239cc6bd4d6f3f083956446ed6760a9e
Emanuel Leite <emanuel.leite10@outlook.com>
2024-01-11 10:47:06 -0600

    Adding .gitignore

--- server/index.js
+++ server/index.js
@@ -4,59 +4,19 @@ const path = require("path");
 const cors = require('cors')
 const multer = require("multer");
 
+// Loads env variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+const imageRoutes = require("./routes/imageOps")
 
 // Whitelist API
 app.use(cors());
 
-// Storage engine 
-const storage = multer.diskStorage({
-    destination: './upload',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+// Routes
+app.use("/", imageRoutes);
 
-const upload = multer({
-    storage: storage,
-})
-
-app.use('/images', express.static('upload'));
-app.post("/upload", upload.single('image'), (req, res) => {
-    res.json({
-        profile_url: `http://localhost:3000/images/${req.file.filename}`
-    })
-})
-
-function errHandler(err, req, res, next) {
-    if (err instanceof multer.MulterError) {
-        res.status(400).json({
-            success: 0,
-            message: err.message
-        });
-    } else if (err instanceof CustomError) {
-        res.status(300).json({
-            success: 0,
-            message: err.message
-        });
-    } else {
-        res.status(500).json({
-            success: 0,
-            message: 'Error interno del servidor'
-        });
-    }
-}
-
-
-app.get("/api", (req, res) => {
-    res.json({"users": ["user1", "user2", "user3"]})
-})
-
-// app.use(errHandler);
-
-app.get('/', (req, res) => {
-    res.send("Server Running...")
-})
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
-    console.log("API running in http://localhost:3000");
-})
+    console.log(`Listen in port http://localhost:${port}`)
+  });
---
+++ server/routes/imageOps.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const multer = require("multer")
+
+// const upload = multer({ storage: multer.memoryStorage() });
+// const { handleUpload, handleUrl } = require("../controllers/imageFuncs");
+
+router.get("/", (req, res) => {
+    res.send("Server running...");
+  });
+
+// router.post("/upload", upload.single("img"), handleUpload);
+
+// router.get("/:id", handleUrl);
+
+module.exports = router;
